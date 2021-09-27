b602e3afa7d54b04b2536797e957dde2ff83fe7a
rhett-williams <gcab022105@coderacademy.edu.au>
2021-09-27 17:16:57 +1000

    cleanandintro

--- Test.rb
+++ Test.rb
@@ -1,30 +1,19 @@
-class Question
-    attr_accessor :prompt, :answer
-    def initialize(prompt, answer)
-         @prompt = prompt
-         @answer = answer
-    end
-end
+# input variables
+name = ""
+years = 0
+MONTHS_PER_YEAR = 12 # a constant
+ 
+# output variable
+months = 0
+ 
+# processing
+print "What is your name? "
+name = gets
+ 
+print "How many years old are you? "
+years = gets
+ 
+months = years * MONTHS_PER_YEAR
 
-p1 = "What color are apples?\n(a) Red/Green\n(b)Orange"
-p2 = "What color are bananas?\n(a) Red/Green\n(b)Yellow"
-
-questions = [
-    Question.new(p1, "a"),
-    Question.new(p2, "b")
-]
-
-def run_quiz(questions)
-    answer = ""
-    score = 0
-    for question in questions
-         puts question.prompt
-         answer = gets.chomp()
-         if answer == question.answer
-              score += 1
-         end
-    end
-    puts "you got #{score} out of #{questions.length()}"
-end
-
-run_quiz(questions)
+puts "#{name}, at #{years} years old, "\
+"you are #{months} months old."
--- terminalapp.rb
+++ terminalapp.rb
@@ -1,40 +1,36 @@
 class Direction
-    attr_accessor :prompt, :answer, :northsouth, :eastwest, :wall1, :wall2, :wall3, :wall4, :wall5, :wall6, :wall7, :wall8, :wall9, :wall10, :wall11, :wall12, :wall13, :wall14, :wall15, :wall16, :wall17, :wall18, :wall19, :wall20, :wall21, :wall22, :wall23
-    def initialize(prompt, answer, northsouth, eastwest, wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13, wall14, wall15, wall16, wall17, wall18, wall19, wall20, wall21, wall22, wall23)
+    attr_accessor :prompt, :answer, :northsouth, :eastwest
+    def initialize(prompt, answer, northsouth, eastwest)
          @prompt = prompt
          @answer = answer
          @northsouth = northsouth
          @eastwest = eastwest
-         @wall1 = wall1
-         @wall2 = wall2
-         @wall3 = wall3
-         @wall4 = wall4
-         @wall5 = wall5
-         @wall6 = wall6
-         @wall7 = wall7
-         @wall8 = wall8
-         @wall9 = wall9
-         @wall10 = wall10
-         @wall11 = wall11
-         @wall12 = wall12
-         @wall13 = wall13
-         @wall14 = wall14
-         @wall15 = wall15
-         @wall16 = wall16
-         @wall17 = wall17
-         @wall18 = wall18
-         @wall19 = wall19
-         @wall20 = wall20
-         @wall21 = wall21
-         @wall22 = wall22
-         @wall23 = wall23
     end
 end
+
+puts "Welcome to:\n░░░░░██╗██╗░░░██╗███╗░░██╗░██████╗░██╗░░░░░███████╗
+░░░░░██║██║░░░██║████╗░██║██╔════╝░██║░░░░░██╔════╝
+░░░░░██║██║░░░██║██╔██╗██║██║░░██╗░██║░░░░░█████╗░░
+██╗░░██║██║░░░██║██║╚████║██║░░╚██╗██║░░░░░██╔══╝░░
+╚█████╔╝╚██████╔╝██║░╚███║╚██████╔╝███████╗███████╗
+░╚════╝░░╚═════╝░╚═╝░░╚══╝░╚═════╝░╚══════╝╚══════╝
+
+░█████╗░██████╗░██╗░░░██╗███████╗███╗░░██╗████████╗██╗░░░██╗██████╗░███████╗
+██╔══██╗██╔══██╗██║░░░██║██╔════╝████╗░██║╚══██╔══╝██║░░░██║██╔══██╗██╔════╝
+███████║██║░░██║╚██╗░██╔╝█████╗░░██╔██╗██║░░░██║░░░██║░░░██║██████╔╝█████╗░░
+██╔══██║██║░░██║░╚████╔╝░██╔══╝░░██║╚████║░░░██║░░░██║░░░██║██╔══██╗██╔══╝░░
+██║░░██║██████╔╝░░╚██╔╝░░███████╗██║░╚███║░░░██║░░░╚██████╔╝██║░░██║███████╗
+╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚══════╝"
+
+name = ""
+print "What is your name? "
+name = gets
+puts "\n#{name}, you find yourself trapped in a jungle with a bunch of crazy stuff going on. You must work your way through the map and attempt to find a helicopter."
 
 p1 = "What direction do you travel in?\n(N)North\n(E)East\n(S)South\n(W)West"
 
 directions = [
-    Direction.new(p1, "N", 0, 0, "0, -3", "-1, -3", "-2, -2", "-3, -1", "-3 0", "-2, 1", "-2, 2", "-2, 3", "-3, 4", "-2, 5", "-1, 6", "0, 6", "1, 6", "2, 5", "3, 5", "4, 4", "3, 3", "2, 2", "2, 1", "3, 0", "3, -1", "2, -2", "1, -3")
+    Direction.new(p1, "N", 0, 0)
 ]
 
 def run_travel(directions)
@@ -61,8 +57,7 @@ def run_travel(directions)
     end
     puts "position #{direction.northsouth}, #{direction.eastwest}"
     case "#{direction.northsouth}, #{direction.eastwest}" 
-    when direction.wall1, direction.wall2, direction.wall3, direction.wall4, direction.wall5, direction.wall6, direction.wall7, direction.wall8, direction.wall9, direction.wall10, direction.wall11, direction.wall12, direction.wall13, direction.wall14, direction.wall15, direction.wall16, direction.wall17, direction.wall18, direction.wall19, direction.wall20, direction.wall21, direction.wall22, direction.wall23 then
-        puts "You've reached a vast ocean with no islands in sight, you can't go this way."
+    when "0, -3", "-1, -3", "-2, -2", "-3, -1", "-3 0", "-2, 1", "-2, 2", "-2, 3", "-3, 4", "-2, 5", "-1, 6", "0, 6", "1, 6", "2, 5", "3, 5", "4, 4", "3, 3", "2, 2", "2, 1", "3, 0", "3, -1", "2, -2", "1, -3" then
         if answer == "N"
             direction.northsouth += -1
         end
@@ -75,9 +70,10 @@ def run_travel(directions)
         if answer == "S"
             direction.northsouth += 1
         end
+        puts "You've reached a vast ocean with no islands in sight, you can't go this way. \nposition #{direction.northsouth}, #{direction.eastwest}"
     end
     if direction.northsouth == 3 && direction.eastwest == 4
-        puts "ligma"
+        puts "Congratulations! you've escaped"
     else
         run_travel(directions)
     end
